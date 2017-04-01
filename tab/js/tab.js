4b3a98053f38872f4aa40533c47ab0301e2663f9
cspark <csparkzxc1@gmail.com>
2017-04-01 17:22:02 +0900

    Modified file

---
+++ tab/js/tab.js
@@ -0,0 +1,10 @@
+function Showtab(val) {
+  for(i=0; i<3; i++) {
+    var tb = document.getElementById('tab_'+ i);
+    if(i != val) {
+      tb.style.display = 'none';
+    } else {
+      tb.style.display = 'block';
+    }
+  }
+};
